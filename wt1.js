76ddd6e72605cb5516ce4467c0d2b6b8f70ce6c6
Smita Patankar <smita.patankar.313@gmail.com>
2017-06-04 10:35:50 -0700

    Update wt1.js

--- wt1.js
+++ wt1.js
@@ -1,4 +1,5 @@
+var util = require('util');
 module.exports = function(ctx, cb) {
-  console.log('Webhook invoked');
-  cb(null, { hello: ctx.data.name || 'Anonymous' });
+  console.log(util.inspect(ctx.body, {depth:null}));
+  cb();
 };
