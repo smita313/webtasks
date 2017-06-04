c3910ddd9861aac20ad492ff040cf1f01a3925a0
Smita Patankar <smita.patankar.313@gmail.com>
2017-06-04 10:40:56 -0700

    Update wt1.js

--- wt1.js
+++ wt1.js
@@ -1,5 +1,8 @@
-var util = require('util');
+//var util = require('util');
+import util from "util";
+
 module.exports = function(ctx, cb) {
-  console.log(util.inspect(ctx.body, {depth:null}));
+  //console.log(util.inspect(ctx.body, {depth:null}));
+  console.log(util.inspect(ctx.body.issue.url));
   cb();
 };
